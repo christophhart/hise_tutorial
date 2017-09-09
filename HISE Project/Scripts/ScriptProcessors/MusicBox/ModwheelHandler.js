a2836bbd672e1c9d767d9d9c00986e1e355cd583
Christoph Hart <christoph-hart@gmx.de>
2017-09-09 20:15:41 +0200

    - deactivated synth sounds outside of mapped range

--- HISE Project/Scripts/ScriptProcessors/MusicBox/ModwheelHandler.js
+++ HISE Project/Scripts/ScriptProcessors/MusicBox/ModwheelHandler.js
@@ -7,16 +7,27 @@ reg modWheelGain = 1.0;
 // This will store the current velocity
 reg currentVelocity = 127.0;
 
-function onNoteOn()
+const var LO_KEY = 50;
+const var HI_KEY = 76;function onNoteOn()
 {
 	currentVelocity = amount.getValue() * Message.getVelocity() * modWheelGain + 
                       (1.0 - amount.getValue()) * Message.getVelocity();
 
     Message.setVelocity(currentVelocity);
+    
+    if(Message.getNoteNumber() < LO_KEY || 
+       Message.getNoteNumber() > HI_KEY)
+	{
+		Message.ignoreEvent(true);
+	}
 }
 function onNoteOff()
 {
-	
+	if(Message.getNoteNumber() < LO_KEY || 
+       Message.getNoteNumber() > HI_KEY)
+	{
+		Message.ignoreEvent(true);
+	}
 }
 function onController()
 {
