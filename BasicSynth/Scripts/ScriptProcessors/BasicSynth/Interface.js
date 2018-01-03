87a020c79eedb4275ef101c434659d05827679b6
Christoph Hart <christoph-hart@gmx.de>
2018-01-03 10:30:25 +0100

    - added basic synth example

---
+++ BasicSynth/Scripts/ScriptProcessors/BasicSynth/Interface.js
@@ -0,0 +1,73 @@
+/** HISE Basic Synth
+*
+*   An example project that shows the usage of HISE for a basic synth.
+*
+*/
+
+// iPad full screen resolution (uses retina automatically)
+Content.makeFrontInterface(1024, 768);
+
+//! =============================================================== Page Handling
+
+// Get these lines by selecting all controls and choose "Create Script Definition for selection"
+const var SynthPageButton = Content.getComponent("SynthPageButton");
+const var PresetButton = Content.getComponent("PresetButton");
+const var SettingsButton = Content.getComponent("SettingsButton");
+const var FXPageButton = Content.getComponent("FXPageButton");
+
+const var SynthPage = Content.getComponent("SynthPage");
+const var FXPage = Content.getComponent("FXPage");
+const var PresetBrowser = Content.getComponent("PresetBrowser");
+const var SettingsPage = Content.getComponent("SettingsPage");
+
+// Put them all in an array for better iterating.
+const var pages = [SynthPage, FXPage, PresetBrowser, SettingsPage];
+
+/** This function iterates the array, hides all pages and only shows the given page.
+*
+*   pageToShow is a reference to the control. Check the onControl callback to see what's going on.
+*/
+inline function handlePages(pageToShow)
+{
+    for(p in pages)
+        p.set("visible", pageToShow == p);
+}
+
+// This page will be shown as default
+// If you're working on another page, just pass this is temporarily
+// so it doesn't switch back when compiling.
+handlePages(SynthPage);
+
+include("CustomCallbacks.js");
+
+
+
+function onNoteOn()
+{
+	
+}
+function onNoteOff()
+{
+	
+}
+function onController()
+{
+	
+}
+function onTimer()
+{
+	
+}
+function onControl(number, value)
+{
+    /*  This callback just calls the page handling function with the respective page.
+    *   The other custom callbacks are implemented in the onInit script.
+    */
+	switch(number)
+    {
+        case SynthPageButton: handlePages(SynthPage); break;
+        case FXPageButton: handlePages(FXPage); break;
+        case SettingsButton: handlePages(SettingsPage); break;
+        case PresetButton: handlePages(PresetBrowser); break;
+    }
+}
