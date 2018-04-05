2e9dbf6e9bb4598c1e485ea7dc4609184417c7d8
Christoph Hart <christoph-hart@gmx.de>
2018-04-05 15:44:40 +0200

    - added WavetableSynth project

---
+++ WavetableSynth/Scripts/ScriptProcessors/WavetableDemo/Interface.js
@@ -0,0 +1,20 @@
+Content.makeFrontInterface(1024, 768);function onNoteOn()
+{
+	
+}
+function onNoteOff()
+{
+	
+}
+function onController()
+{
+	
+}
+function onTimer()
+{
+	
+}
+function onControl(number, value)
+{
+	
+}
