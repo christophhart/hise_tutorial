e3ab4a1cd73d0377d2b32b4d7c670de34aa2bafd
Christoph Hart <christoph-hart@gmx.de>
2023-07-12 14:48:52 +0200

    - updated Wavetable synth project

--- WavetableSynth/Scripts/ScriptProcessors/WavetableDemo/Interface.js
+++ WavetableSynth/Scripts/ScriptProcessors/WavetableDemo/Interface.js
@@ -1,20 +1,42 @@
-Content.makeFrontInterface(1024, 768);function onNoteOn()
+Content.makeFrontInterface(1024, 768);
+
+const var WavetableSelector = Content.getComponent("WavetableSelector");
+
+WavetableSelector.set("items", Engine.getWavetableList().join("\n"));
+
+const var WavetableSynthesiser1 = Synth.getChildSynth("Wavetable Synthesiser1");
+
+inline function onWavetableSelectorControl(component, value)
+{
+	if(value > 0)
+	{
+		WavetableSynthesiser1.setAttribute(WavetableSynthesiser1.LoadedBankIndex, value);
+	}
+};
+
+Content.getComponent("WavetableSelector").setControlCallback(onWavetableSelectorControl);
+
+
+
+
+function onNoteOn()
 {
 	
 }
-function onNoteOff()
+ function onNoteOff()
 {
 	
 }
-function onController()
+ function onController()
 {
 	
 }
-function onTimer()
+ function onTimer()
 {
 	
 }
-function onControl(number, value)
+ function onControl(number, value)
 {
 	
 }
+ 
